932f0efad613a54dad70587ee1b0bb33885fcd85
ivyONS <26869198+ivyONS@users.noreply.github.com>
2020-03-02 11:01:26 +0000

    passport reference to other

--- scripts/delivery3.py
+++ scripts/delivery3.py
@@ -33,7 +33,7 @@ def generate_relationships(census_table):
 
 def add_passport(census_table, passport_file = '../data/other_passport.csv'):
     code_passport = pd.read_csv(passport_file, header = None, dtype = {0 : str})[0]
-    output = pd.DataFrame([{'Passport': random.choice(['-7', '-9', '1000', '1100', '1010', '1110', '0100', '0110', '0010', '0001']),
+    output = pd.DataFrame([{'Passport': random.choice(['-7', '-9', '1000', '1000', '1000', '1100', '1010', '1110', '0100', '0110', '0010', '0001']),
                'Other_Passport_Held': random.choice(['-7', '-9'])}  for x in  range(census_table.shape[0])])
     output.loc[output.Passport.isin(['1010', '1110', '0110', '0010']), 'Other_Passport_Held'] =\
         [random.choice(['-7', '-9']+ code_passport.tolist()) for x in range(sum(output.Passport.isin(['1010', '1110', '0110', '0010'])))]
